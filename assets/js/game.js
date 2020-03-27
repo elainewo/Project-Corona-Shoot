77db3f0875c0415ac78e03e152f026732c866717
elaine <yw.4contact@gmail.com>
2020-03-27 10:43:46 +0100

    ProjectCoronaShoot v.0.0.3

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,15 @@
+let t = 0;
+let txt = 'The world is near its end. The world is almost done. China has got it under control, Germany is still struggling but optimistic and USA is going to lose. Can you help the countries to overcome the apocalypse? Choose toiletpaper as your main weapon and fight!';
+let speed = 100;
+function typeWriter() {
+    if (t < txt.length) {
+        document.getElementById("delta1").innerHTML += txt.charAt(t);
+        t++;
+        setTimeout(typeWriter, speed);
+    }
+}
+typeWriter()
+
 // ----------------------------------------- Code für grünen Virus -----------------------------------------
 let i = 0;
 let TimeId1 = setInterval(() => {
