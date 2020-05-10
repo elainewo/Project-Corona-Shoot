60fb6ee955b380fb6817699a232643df2fb844bf
elainewo <yw.4contact@gmail.com>
2020-05-10 17:28:59 +0200

    update

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,13 +1,14 @@
 // ---------------------------------------- TypeWriter ----------------------------------------
 let t = 0;
-let txt = 'The world is near its end. The world is almost done. China has got it under control, Germany is still struggling but optimistic and USA is going to lose. Can you help the countries to overcome the apocalypse? Choose toiletpaper as your main weapon and fight!';
-let speed = 50;
+let txt =
+  "The world is near its end. The world is almost done. China has got it under control, Germany is still struggling but optimistic and USA is going to lose. Can you help the countries to overcome the apocalypse? Choose toiletpaper as your main weapon and fight!";
+let speed = 35;
 function typeWriter() {
-    if (t < txt.length) {
-        document.getElementById("delta1").innerHTML += txt.charAt(t);
-        t++;
-        setTimeout(typeWriter, speed);
-    }
+  if (t < txt.length) {
+    document.getElementById("delta1").innerHTML += txt.charAt(t);
+    t++;
+    setTimeout(typeWriter, speed);
+  }
 }
-typeWriter()
+typeWriter();
 // ---------------------------------------- TypeWriter ----------------------------------------
